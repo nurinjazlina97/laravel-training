b9a027c1cb92afa9e086efc982577efa2d218e4d
nurinjazlina97 <nurynn.nj@gmail.com>
2023-03-09 15:16:49 +0800

    schedule index

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Schedule;
 
 class ScheduleController extends Controller
 {
-    //
+    public function index()
+    {
+        $schedules = Schedule::all();
+
+        return view('schedules.index', compact('schedules'));
+    }
 }
---
+++ resources/views/schedules/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Schedule Index') }}</div>
+
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Title</th>
+                                    <th>Description</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($schedules as $schedule)
+                                    <tr>
+                                        <td>{{ $schedule->id }}</td>
+                                        <td>{{ $schedule->title }}</td>
+                                        <td>{{ $schedule->description }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
